5d3c6c3dc5828de59b05c75131fcb032fcd32cf0
Shikhkarim <71795994+memmedov-karim@users.noreply.github.com>
2026-01-12 14:09:11 +0400

    Delete dist directory

--- dist/index.d.ts
+++
@@ -1,3 +0,0 @@
-export { AzericardPaymentClient } from "./payment-client";
-export { AZERICARD_PROD_URL, AZERICARD_TEST_URL, generatePaymentForm, generateBadRequestHtml, } from "./html";
-export type { AzericardKeyConfig, AzericardMerchantConfig, CreatePaymentInput, GenerateFormOptions, KeySource, PaymentPayload, VerifyResponseInput, } from "./types";
